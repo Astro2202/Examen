952ad233aaaadc7393f51f5bf9377fcd95ea27aa
wouter <vannuetenwouter@gmail.com>
2020-01-20 09:54:57 +0100

    initial product.service.ts commit

---
+++ product.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { Product } from './product';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable()
+export class ProductService {
+
+   private productsServiceURI: string = 'http://localhost:3000/products'
+
+   constructor(private http: HttpClient) { }
+
+
+   getAllItems(): Observable<Product[]> {
+      return this.http.get<Product[]>(this.productsServiceURI);
+   }
+
+}
